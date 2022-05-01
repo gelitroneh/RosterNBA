53e84e0f9f2f68fd38af289ac7e11faa4976984b
ander.pardo <ander.pardo@alumnos.uneatlantico.es>
2022-05-01 21:53:52 +0200

    Instructions

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Roster NBA 2021-2022
+
+For development actions
+# Xampp
+1-Start apache and MySQL
+2-Insert the project folder in htdocs
+3-Open http://localhost/RosterNBA/
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Roster NBA</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <?php 
+        echo "Hello world\n";//No retorna
+        print "Hello world\n";//Retorna 1
+        //Variable
+        $sentence = "Greetings, Earthlings!";
+        $number = 12.3;
+        echo $sentence;
+        echo "Esto es un numero -> $number";
+
+        //CONSTANTE (no se puede cambiar)
+        define("PI", 3.14);
+    ?>
+
+    
+</body>
+</html>
